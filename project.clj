120ab7f543bc601b2abc91e827b245aba9a70506
Johan Thoren <jthoren@itrsgroup.com>
2020-12-11 09:45:35 +0100

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject advent_2020 "0.1.0-SNAPSHOT"
+  :description "Advent of code - 2020"
+  :url "https://github.com/johanthoren/advent_of_code_2020"
+  :license {:name "ISC License"
+            :url "none"
+            :year 2020
+            :key "isc"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/tools.cli "1.0.194"]]
+  :main ^:skip-aot advent-2020.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
---
+++ test/advent_2020/core_test.clj
@@ -0,0 +1,3 @@
+(ns advent-2020.core-test
+  (:require [clojure.test :refer :all]
+            [advent-2020.core :refer :all]))
