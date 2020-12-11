ec8fc31897e2d658df86fc64386309ad49f13d69
Johan Thoren <jthoren@itrsgroup.com>
2020-12-11 10:40:23 +0100

    Added solution for part 1 of day 1.

--- src/advent_2020/core.clj
+++ src/advent_2020/core.clj
@@ -1,5 +1,21 @@
 (ns advent-2020.core
+  (:require [clojure.tools.cli :refer [parse-opts]]
+            [clojure.string :as str])
   (:gen-class))
+
+(def input-1
+  (map read-string (str/split-lines (slurp "resources/input-1.txt"))))
+
+(defn add-to-2020
+  []
+  ;;(reduce * (set (remove nil? (flatten (for [a input-1] (map #(when (= 2020 (+ a %)) [a %]) input-1)))))))
+  (as-> input-1 <>
+      (for [a <>] (map #(when (= 2020 (+ a %)) [a %]) <>))
+      (flatten <>)
+      (remove nil? <>)
+      (set <>)
+      (reduce * <>)))
+
 
 (defn -main
   "I don't do a whole lot ... yet."
