8835fab8421a76e5da265729b873e906ae508b69
Johan Thoren <johan@thoren.xyz>
2020-12-12 23:04:38 +0100

    Added solutions for day 4 and part 1 of day 5.

--- src/advent_2020/core.clj
+++ src/advent_2020/core.clj
@@ -124,10 +124,184 @@
   (println "  A:" (tree-hit-count 3 1))
   (println "  B:" tree-hit-multiple))
 
+(def input-4
+  (as-> (slurp "resources/input-4.txt") <>
+    (str/split <> #"\n\n")
+    (map #(str/replace % #"\n" " ") <>)
+    (map str/trimr <>)
+    (map #(str/split % #" ") <>)
+    (for [s <>] (map #(str/split % #":") s))
+    (for [s <>] (zipmap (map #(keyword (first %)) s) (map #(last %) s)))))
+
+(defn valid-nof?
+  [passport]
+  (when (= 7 (count (keys (dissoc passport :cid))))
+    true))
+
+(defn is-year?
+  [s]
+  (when (and (= 4 (count s))
+             (empty? (re-find #"[^0-9]" s)))
+    true))
+
+(defn valid-byr?
+  [passport]
+  (let [byr (:byr passport)]
+    (when (and (is-year? byr)
+               (<= 1920 (read-string byr) 2002))
+      true)))
+
+(defn valid-iyr?
+  [passport]
+  (let [iyr (:iyr passport)]
+    (when (and (is-year? iyr)
+               (<= 2010 (read-string iyr) 2020))
+      true)))
+
+(defn valid-eyr?
+  [passport]
+  (let [eyr (:eyr passport)]
+    (when (and (is-year? eyr)
+               (<= 2020 (read-string eyr) 2030))
+      true)))
+
+(defn hgt-within-range?
+  [height unit]
+  (case unit
+    "cm" (when (<= 150 height 193) true)
+    "in" (when (<= 59 height 76) true)
+    nil))
+
+(defn valid-hgt?
+  [passport]
+  (let [h (:hgt passport)]
+    (when h
+      (let[ending (re-find #"in|cm" h)
+           measure (read-string (if (seq ending)
+                                  (first (str/split h (re-pattern ending)))
+                                  h))]
+        (when (and (not (empty? ending)) (hgt-within-range? measure ending))
+          true)))))
+
+(defn valid-hcl?
+  [passport]
+  (let [c (:hcl passport)]
+    (when (and (= 7 (count c))
+               (= \# (first c))
+               (empty? (re-find #"[^0-9a-f]" (subs c 1))))
+      true)))
+
+(def valid-ecls ["amb" "blu" "brn" "gry" "grn" "hzl" "oth"])
+
+(defn valid-ecl?
+  [passport]
+  (when (seq (filter #(re-find
+                       (re-pattern (str "^" (:ecl passport) "$"))
+                       %)
+                     valid-ecls))
+    true))
+
+(defn valid-pid?
+  [passport]
+  (let [p (:pid passport)]
+    (when (and (= 9 (count p))
+               (empty? (re-find #"[^0-9]" p)))
+      true)))
+
+(defn count-all-passports-with-valid-nof
+  []
+  (count (remove nil? (for [p input-4] (valid-nof? p)))))
+
+(defn all-valid-passports
+  []
+  (remove nil? (for [p input-4]
+                 (when (and (valid-nof? p)
+                            (valid-byr? p)
+                            (valid-iyr? p)
+                            (valid-eyr? p)
+                            (valid-hgt? p)
+                            (valid-hcl? p)
+                            (valid-ecl? p)
+                            (valid-pid? p))
+                   p))))
+
+(defn all-invalid-passports
+  []
+  (remove nil? (for [p input-4]
+                 (when-not (and (valid-nof? p)
+                                (valid-byr? p)
+                                (valid-iyr? p)
+                                (valid-eyr? p)
+                                (valid-hgt? p)
+                                (valid-hcl? p)
+                                (valid-ecl? p)
+                                (valid-pid? p))
+                   p))))
+
+
+(defn solve-day-4
+  []
+  (println "Solutions for Day 4:")
+  (println "  A:" (count-all-passports-with-valid-nof))
+  (println "  B:" (count-all-valid-passports)))
+
+(defn fblr-to-01
+  [s]
+  (for [x s]
+    (case x
+      \F 0
+      \B 1
+      \L 0
+      \R 1
+      nil)))
+
+(def input-5
+  (as-> (slurp "resources/input-5.txt") <>
+    (str/split-lines <>)
+    (for [s <>] [(subs s 0 7) (subs s 7)])))
+
+
+(def binary-input-5
+  (for [s input-5] (flatten (map fblr-to-01 s))))
+
+(defn find-row-or-seat
+  ([b range]
+   (let [range-length (count range)]
+     (if (>= 1 range-length)
+       (first range)
+       (if (= 0 (first b))
+         (find-row-or-seat (drop 1 b) (drop-last (/ range-length 2) range))
+         (find-row-or-seat (drop 1 b) (drop (/ range-length 2) range))))))
+  ([b]
+   (if (= 3 (count b))
+     (find-row-or-seat b (range 0 7))
+     (find-row-or-seat b (range 0 128)))))
+
+(defn find-all-rows-and-seats
+  []
+  (for [t binary-input-5]
+    [(find-row-or-seat t) (find-row-or-seat (drop 7 t))]))
+
+(defn calculate-seat-ids
+  []
+  (for [i (find-all-rows-and-seats)]
+    (+ (* 8 (first i)) (last i))))
+
+(defn highest-seat-id
+  []
+  (last (sort (calculate-seat-ids))))
+
+(defn solve-day-5
+  []
+  (println "Solutions for Day 5:")
+  (println "  A:" (highest-seat-id)))
+
 (defn -main
   [d]
   (case (read-string d)
     1 (solve-day-1)
     2 (solve-day-2)
     3 (solve-day-3)
+    4 (solve-day-4)
+    5 (solve-day-5)
     (die 1 "Incorrect day provided.")))
